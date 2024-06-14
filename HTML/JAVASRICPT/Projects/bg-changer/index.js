c761260565f23a474dff2a7093143f08e2d7f59f
thimothi6 <ms86882185@gmail.com>
2024-06-14 18:00:38 +0530

    update

--- HTML/JAVASRICPT/Projects/bg-changer/index.js
+++ HTML/JAVASRICPT/Projects/bg-changer/index.js
@@ -1,8 +1,8 @@
-// let colors= ["red", "black","blue","yellow","pink","orange"]
+ let colors= ["red", "black","blue","yellow","pink","orange"]
 
 function change()
 {
-    // document.body.style.backgroundColor= colors[Math.floor(Math.random()*6)]
+    document.body.style.backgroundColor= colors[Math.floor(Math.random()*6)]
     const rc = Math.round(Math.random()*10000000).toString(16)
     document.body.style.backgroundColor = "#" +rc;
 }
